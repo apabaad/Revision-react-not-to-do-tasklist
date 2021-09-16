09fa5ce26b73d4a4e4a952f5025b7b22a119bb82
Rupesh Adhikari <apabaad@yahoo.com>
2021-09-16 12:22:29 +1000

    add task form done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AddTaskForm } from './components/add-task-form/AddTaskForm';
+import { TaskList } from './components/task-list/TaskList';
+import { useState } from 'react';
 
 function App() {
-  return <div> hello </div>;
+  const [tasks, setTasks] = useState([]);
+
+  const handleOnSubmit = (data) => {
+    setTasks([...tasks, data]);
+  };
+
+  return (
+    <div className="wrapper text-center">
+      <Container>
+        <Row className="mt-5 ">
+          <Col>
+            <h1>Not To Do Task List</h1>
+          </Col>
+        </Row>
+        <hr />
+        <AddTaskForm handleSubmit={handleOnSubmit} />
+        <hr />
+        <Row>
+          <Col md="6">
+            <h2>Task List</h2>
+          </Col>
+          <Col md="6">
+            <h2>Bad Task List</h2>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/add-task-form/AddTaskForm.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+
+const initialFormData = {
+  task: '',
+  hr: 0,
+};
+
+export const AddTaskForm = ({ handleSubmit }) => {
+  const [formData, setFormData] = useState();
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(formData);
+  };
+
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Row>
+        <Col md={7}>
+          <Form.Control
+            name="task"
+            placeholder="task"
+            onChange={handleOnChange}
+            required
+          />
+        </Col>
+        <Col>
+          <Form.Control
+            name="hr"
+            type="number"
+            placeholder="hour"
+            onChange={handleOnChange}
+            required
+          />
+        </Col>
+        <Col>
+          <Button type="submit">Add Task</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
